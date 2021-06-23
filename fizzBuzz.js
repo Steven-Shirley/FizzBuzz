04389293458e2408c3d727163d1bbe5e9064f589
Steven Shirley <steven.shirley@axiell.com>
2021-06-23 15:22:46 +0100

    adds FizzBuzz code

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+const fizzBuzz = (i) => {
+    for (i=1; i<=100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz")
+        }if (i % 5 == 0) {
+            console.log("Buzz")
+        }else if (i % 3 == 0) {
+            console.log("Fizz")
+        }else {
+            console.log(i)
+        };
+        i
+    };
+};
+
+fizzBuzz();
